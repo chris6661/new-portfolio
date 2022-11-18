80c21ad2b079049c1cae8fdd3e0b0e2acf68137c
chris6661 <74381468+chris6661@users.noreply.github.com>
2022-11-17 21:18:05 -0600

    added baseline for about me section

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -4,10 +4,62 @@ import ScrollService from "../../utilities/ScrollService"
 import Animations from "../../utilities/Animations"
 
 export default function AboutMe(props) {
+
+    let fadeInScreenHandler = (screen) => {
+        if(screen.fadeScreen !== props.id)
+        return
+        Animations.animations.fadeInScreen(props.id)
+    }
+
+    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+    const SCREEN_CONSTANTS = {
+        description: "full stack web developer with knowledge of MERN stack full stack web developer with knowledge of MERN stackfull stack web developer with knowledge of MERN stackfull stack web developer with knowledge of MERN stackfull stack web developer with knowledge of MERN stackfull stack web developer with knowledge of MERN stack. full stack web developer with knowledge of MERN stackfull stack web developer with knowledge of MERN stack.",
+        // list of highlights
+        highlights: {
+            bullets: [
+                "Full Stack Web Developer",
+                "Front End web development",
+                "React", 
+                "SQL/Database Management"
+            ],
+            heading: "Here are a few highlights"
+        }
+    }
+    const renderHighlight = () => {
+        return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+                <div className='highlisghts' key ={i}>
+                    <div className='highlight-blob'></div>
+                        <span>{value}</span>
+                    </div>
+            ));
+    };
+
   return (
-    <div className='about-me-container screen-container'>
+    <div className='about-me-container screen-container' id = {props.id || ""}>
         <div className='about-me-parent'>
             <ScreenHeading title={"About Me"} subHeading = {"Why Choose Me?"}/>
+            <div className='about-me-card'>
+                <div className='about-me-profile'></div>
+                <div className='about-me-details'>
+                    <span className='about-me-description'>{SCREEN_CONSTANTS.description}</span>
+                    <div className='about-me-highlights'>
+                        <div className='highlight-heading'> 
+                            <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+                        </div>
+                        {renderHighlight()}
+                    </div>
+                    <div className='about-me-options'>
+                    <button className="btn primary-btn">
+              {" "}
+              Hire Me {" "}
+            </button>
+            <a href = "chrishrutkay.pdf" download = "chrishrutkay.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+                    </div>   
+                </div>
+            </div>
         </div>
     </div>
   )
