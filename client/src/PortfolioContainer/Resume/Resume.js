76687bade043278f37357891dd12be82ae00834c
chris6661 <74381468+chris6661@users.noreply.github.com>
2022-11-25 23:28:26 -0600

    added more in to resume componenet

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -13,12 +13,34 @@ export default function Resume(props) {
         <div className='resume-heading'>
             <div className='resume-main-heading'>
                 <div className='heading-bullet'>
-
+                    <span>{props.heading ? props.heading : ""}</span>
+                    {props.fromDate && props.toDate ? (
+                        <div className='heading-date'>
+                            {props.fromDate + "_" + props.toDate}
+                        </div>
+                    ) : (
+                        <div></div>
+                    )}
+                </div>
+                <div className='resume-sub-heading'>
+                    <span>{props.subHeading ? props.subHeading : ''}</span>
+                </div>
+                <div className='resume-heading-description'>
+                    <span>{props.description ? props.description : ''}</span>
                 </div>
             </div>
         </div>
 
     }
+
+    const resumeBullets = [
+        {label : "Education", logoSrc: "education.svg"}, 
+        {label: "Work History", logoSrc: "work-history.svg"},
+        {label: "Programming Skills", logoSrc: "programming-skills.svg"}, 
+        {label: "Projects", logoSrc: "projects.org"}, 
+        {label: "Interests", logoSrc: "interests.svg"}
+        
+    ]
 
     let fadeInScreenHandler = (screen) => {
         if(screen.fadeScreen !== props.id)
