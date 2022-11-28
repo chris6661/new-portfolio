3179a4213b50f80464e734e98ac1fda89e387447
chris6661 <74381468+chris6661@users.noreply.github.com>
2022-11-28 14:11:42 -0600

    added in resume filler text, added differnet sections of resume componenet

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -38,8 +38,81 @@ export default function Resume(props) {
         {label: "Work History", logoSrc: "work-history.svg"},
         {label: "Programming Skills", logoSrc: "programming-skills.svg"}, 
         {label: "Projects", logoSrc: "projects.org"}, 
-        {label: "Interests", logoSrc: "interests.svg"}
-        
+        {label: "Interests", logoSrc: "interests.svg"}  
+    ];
+
+    const programmingSkillDetails = [
+        {skill: "JavaScript", ratingPercentage: 75},
+        {skill: "React JS", ratingPercentage: 75},
+        {skill: "Express JS", ratingPercentage: 75},
+        {skill: "Node JS", ratingPercentage: 75},
+        {skill: "Mongo DB", ratingPercentage: 75},
+        {skill: "HTML", ratingPercentage: 75},
+        {skill: "CSS", ratingPercentage: 75},
+        {skill: "Python", ratingPercentage: 75}
+    ];
+
+    const projectDetails = [
+        {
+            title: "Portfolio", 
+            duration: {fromDate: "2020", toDate: "2020"}, 
+            description: "Portfolio website to showcase my personal work",
+            subheading: "Built with: React JS, Bootstrap"
+        },
+        {
+            title: "Portfolio", 
+            duration: {fromDate: "202", toDate: "2021"}, 
+            description: "Portfolio website t",
+            subheading: "Built with: React JS, html"
+        },
+        {
+            title: "Portfolio", 
+            duration: {fromDate: "2022", toDate: "2022"}, 
+            description: "Portfolio websitewcase my personal work",
+            subheading: "Built with: React JS, css"
+        }
+    ];
+
+    const resumeDetails = [
+        <div className='resume-screen-container' key = "education">
+            <ResumeHeading
+            heading = {"University of Texas at Austin"}
+            subHeading = {"Coding Boot Camp"}
+            fromDate = {"November 2020"}
+            toDate = {"June 2021"}
+            // do i want to add in high school??
+            />
+        </div>,
+        <div className='resume-screen-container' key = "work-experience">
+            {/* work experience section */}
+            <ResumeHeading
+            heading = {"Hopdoddy Burger Bar"}
+            subHeading = {"ospitality Manager"}
+            fromDate = {"April 2021"}
+            toDate = {"Present"}
+            />
+
+            <div className='experience-description'>
+                <span className='resume-description-text'>
+                    enter job description here, adding in filler words while building, need to change later
+                </span>
+            </div>
+
+            <div className='experience-description'>
+                <span className='resume-description-text'>
+                    -Accomplishents in current role go here, add in some more filler text to render to page
+                </span>
+                <br />
+                <span className='resume-description-text'>
+                    -Accomplishents in current role go here, add in some more filler text to render to page
+                </span>
+                <br />
+                <span className='resume-description-text'>
+                    -Accomplishents in current role go here, add in some more filler text to render to page
+                </span>
+            </div>
+
+        </div>
     ]
 
     let fadeInScreenHandler = (screen) => {
